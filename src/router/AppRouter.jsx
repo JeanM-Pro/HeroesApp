3ffc4fbdad6127b859b52bc3abaaeb132e6fd28c
Jean Centeno <jeancenteno54@gmail.com>
2022-08-15 18:23:06 -0300

    se aplicaron las rutas publicas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,12 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes";
 import { PrivateRoutes } from "./PrivateRoutes";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
+        {/* <Route path="login" element={<LoginPage />} /> */}
+
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
 
         <Route
           path="/*"
